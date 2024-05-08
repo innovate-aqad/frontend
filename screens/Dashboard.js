77ee5d5271b66420b19f0ba9c411e72316ae14d5
tanujkumar11 <tanujkumar11>
2024-05-09 01:07:01 +0530

    changes for graph

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -11,6 +11,84 @@ import {
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import CarouselHome from '../Shared/Carousel';
+import { LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph} from 'react-native-chart-kit';
+import { data, contributionData, pieChartData, progressChartData } from './data'
+import LiveOrder from '../Shared/LiveOrder';
+const chartConfigs = [
+  {
+    backgroundColor: '#000000',
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientTo: '#08130D',
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    style: {
+      borderRadius: 16
+    }
+  },
+  {
+    backgroundColor: '#022173',
+    backgroundGradientFrom: '#022173',
+    backgroundGradientTo: '#1b3fa0',
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+      borderRadius: 16
+    }
+  },
+  {
+    backgroundColor: '#ffffff',
+    backgroundGradientFrom: '#ffffff',
+    backgroundGradientTo: '#ffffff',
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`
+  },
+  {
+    backgroundColor: '#26872a',
+    backgroundGradientFrom: '#43a047',
+    backgroundGradientTo: '#66bb6a',
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+      borderRadius: 16
+    }
+  },
+  {
+    backgroundColor: '#000000',
+    backgroundGradientFrom: '#000000',
+    backgroundGradientTo: '#000000',
+    color: (opacity = 1) => `rgba(${255}, ${255}, ${255}, ${opacity})`
+  }, {
+    backgroundColor: '#0091EA',
+    backgroundGradientFrom: '#0091EA',
+    backgroundGradientTo: '#0091EA',
+    color: (opacity = 1) => `rgba(${255}, ${255}, ${255}, ${opacity})`
+  },
+  {
+    backgroundColor: '#e26a00',
+    backgroundGradientFrom: '#fb8c00',
+    backgroundGradientTo: '#ffa726',
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+      borderRadius: 16
+    }
+  },
+  {
+    backgroundColor: '#b90602',
+    backgroundGradientFrom: '#e53935',
+    backgroundGradientTo: '#ef5350',
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+      borderRadius: 16
+    }
+  },
+  {
+    backgroundColor: '#ff3e03',
+    backgroundGradientFrom: '#ff3e03',
+    backgroundGradientTo: '#ff3e03',
+    color: (opacity = 1) => `rgba(${0}, ${0}, ${0}, ${opacity})`
+  }
+]
+
 
 // FontAwesome6
 
@@ -19,7 +97,7 @@ export default function Dashboard() {
   const handleSearch = () => {};
   return (
     <View className="flex flex-col pb-20">
-      <ScrollView style={styles.scrollView}>
+      {/* <ScrollView style={styles.scrollView}>
         <View className="p-3">
           <View className="flex flex-row w-full bg-gray-200 rounded-full">
             <TouchableOpacity className="w-[50%] h-[39px]" style={styles.button}>
@@ -43,7 +121,6 @@ export default function Dashboard() {
               placeholder="Search..."
               underlineColorAndroid="transparent"
               value={searchText}
-              // textColor='#cbcbcb'
               placeholderTextColor={"#cbcbcb"}
               keyboardType="default"
               disableFullscreenUI={true}
@@ -85,7 +162,6 @@ export default function Dashboard() {
                 style={styles.tinyLogo}
                 source={require('../Assets/image/drawable-hdpi/group_186.png')}
               />
-              {/* </Text> */}
             </View>
           </View>
           <View className="flex flex-col items-center">
@@ -200,7 +276,325 @@ export default function Dashboard() {
           </View>
         </View>
         <CarouselHome />
-      </ScrollView>
+      </ScrollView> */}
+      <View className="relative top-0 flex flex-row items-center p-5 bg-white">
+        <Text
+          className="flex justify-center w-[80%] text-center text-[#00274d]"
+          style={{fontFamily: 'Poppins-Bold'}}>
+          PRODUCTS
+        </Text>
+      </View>
+      <View className="p-3 px-5 gap-y-3">
+        <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            placeholder="Search..."
+            underlineColorAndroid="transparent"
+            value={searchText}
+            placeholderTextColor={'#cbcbcb'}
+            keyboardType="default"
+            disableFullscreenUI={true}
+            onChangeText={text => setSearchText(text)}
+            onSubmitEditing={handleSearch}
+          />
+
+          <TouchableOpacity onPress={handleSearch}>
+            <AntDesign name="search1" size={24} color="#cbcbcb" />
+          </TouchableOpacity>
+        </View>
+          <View className="flex flex-row justify-between px-3">
+
+         
+          <View style={{ backgroundColor: '#fff', padding: 16, borderRadius: 10, shadowColor: '#000', shadowOpacity: 0.2,shadowRadius: 10, }}>
+                <Text style={{ fontSize: 12,fontWeight: 'bold',marginBottom: 8,}}>Sales Today</Text>
+                <View className="flex flex-row">
+                  <Text style={{ fontSize: 14,color: '#666',fontWeight:"bold"  }}>14570</Text>
+                  <Text style={{ fontSize: 8,color: '#666',marginLeft:2  }}>AED</Text>
+                </View>
+                <View className="flex flex-row">
+                  <Text style={{fontSize: 14,color: '#999',}}>2.3%</Text>
+                  <Text>|</Text>
+                  <Text style={{fontSize: 14,color: '#999',}}>than last year</Text>
+
+                </View>
+          </View>
+          <View style={{ backgroundColor: '#fff', padding: 16, borderRadius: 10, shadowColor: '#000', shadowOpacity: 0.2,shadowRadius: 10, }}>
+                <Text style={{ fontSize: 12,fontWeight: 'bold',marginBottom: 8,}}>Sales Today</Text>
+                <View className="flex flex-row">
+                  <Text style={{ fontSize: 14,color: '#666',fontWeight:"bold"  }}>14570</Text>
+                  <Text style={{ fontSize: 8,color: '#666',marginLeft:2  }}>AED</Text>
+                </View>
+                <View className="flex flex-row">
+                  <Text style={{fontSize: 14,color: '#999',}}>2.3%</Text>
+                  <Text>|</Text>
+                  <Text style={{fontSize: 14,color: '#999',}}>than last year</Text>
+
+                </View>
+          </View>
+          </View>
+          <View className="flex flex-row justify-between px-3">         
+          <View style={{ backgroundColor: '#fff', padding: 16, borderRadius: 10, shadowColor: '#000', shadowOpacity: 0.2,shadowRadius: 10, }}>
+                <Text style={{ fontSize: 12,fontWeight: 'bold',marginBottom: 8,}}>Sales Today</Text>
+                <View className="flex flex-row">
+                  <Text style={{ fontSize: 14,color: '#666',fontWeight:"bold"  }}>14570</Text>
+                  <Text style={{ fontSize: 8,color: '#666',marginLeft:2  }}>AED</Text>
+                </View>
+                <View className="flex flex-row">
+                  <Text style={{fontSize: 14,color: '#999',}}>2.3%</Text>
+                  <Text>|</Text>
+                  <Text style={{fontSize: 14,color: '#999',}}>than last year</Text>
+
+                </View>
+          </View>
+          <View style={{ backgroundColor: '#fff', padding: 16, borderRadius: 10, shadowColor: '#000', shadowOpacity: 0.2,shadowRadius: 10, }}>
+                <Text style={{ fontSize: 12,fontWeight: 'bold',marginBottom: 8,}}>Sales Today</Text>
+                <View className="flex flex-row">
+                  <Text style={{ fontSize: 14,color: '#666',fontWeight:"bold"  }}>14570</Text>
+                  <Text style={{ fontSize: 8,color: '#666',marginLeft:2  }}>AED</Text>
+                </View>
+                <View className="flex flex-row">
+                  <Text style={{fontSize: 14,color: '#999',}}>2.3%</Text>
+                  <Text>|</Text>
+                  <Text style={{fontSize: 14,color: '#999',}}>than last year</Text>
+
+                </View>
+          </View>
+          </View>
+
+        {/* <LiveOrder/> */}
+          <View className="flex flex-row h-fit items-center w-[100%] !justify-between mt-2 px-0 text-black bg-white rounded-xl shadow">
+            <View className="py-1">
+            <LineChart
+              data={{
+                labels: ['Jul', 'Aug', 'Set', 'Oct',"Nov"],
+                datasets: [
+                  {
+                    data: [
+                      10,
+                      30,
+                      50,
+                      70,
+                      50,
+                    ],
+                    color:()=>"#21d59b",
+                    
+                    
+                  },
+                  {
+                    data: [
+                      30,
+                      20,
+                      40,
+                      30,
+                      20,
+                    ],
+                    color:()=>"#0058ff",
+                    
+                  },
+                  {
+                    data: [
+                      70,
+                      50,
+                      60,
+                      70,
+                      80,
+                    ],
+                    color:()=>"#ffc700",
+                    
+                  },
+                  {
+                    data: [
+                      77,
+                      82,
+                      70,
+                      80,
+                      90,
+                    ],
+                    color:()=>"#ccd7e3",
+                    
+                  },
+                ],
+                
+              }}
+              
+              width={250} // from react-native
+              height={144}
+              
+            
+              yAxisLabel=""
+              withShadow={false}
+              yAxisSuffix=""
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: 'whihte',
+                backgroundGradientFrom: 'white',
+                backgroundGradientTo: 'white',
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(100, 100, 100, ${opacity})`,
+                // labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                  borderRightWidth:3
+                },
+                propsForDots: {
+                  r: '0',
+                  strokeWidth: '0',
+                  stroke: 'white',
+                },
+              }}
+              bezier
+              style={{
+                marginVertical: 0,
+                borderRadius: 16,
+                paddingBottom:4,
+                
+              }}
+            />
+            </View>
+            <View className="flex flex-col items-center w-20 pr-3">
+              <Text className="text-[11px] text-[#7e84a3]">Supermarket Product</Text>
+              <Text className="text-[11px] text-[#7e84a3]">Supermarket Product</Text>
+              <Text className="text-[11px] text-[#7e84a3]">Supermarket Product</Text>
+              <Text className="text-[11px] text-[#7e84a3]">Supermarket Product</Text>
+            </View>
+          </View>
+          {chartConfigs.map(chartConfig => {
+          const labelStyle = {
+            color: chartConfig.color(),
+            marginVertical: 10,
+            textAlign: 'center',
+            fontSize: 16
+          }
+          const graphStyle = {
+            marginVertical: 8,
+            ...chartConfig.style
+          }
+          return (
+            <ScrollView
+              key={Math.random()}
+              style={{
+                backgroundColor: chartConfig.backgroundColor
+              }}
+            >
+              <Text style={labelStyle}>Bezier Line Chart</Text>
+              <LineChart
+                data={data}
+                width={190}
+                height={190}
+                chartConfig={chartConfig}
+                bezier
+                style={graphStyle}
+              />
+              <Text style={labelStyle}>Progress Chart</Text>
+              <ProgressChart
+                data={progressChartData}
+                width={190}
+                height={190}
+                chartConfig={chartConfig}
+                style={graphStyle}
+              />
+              <Text style={labelStyle}>Bar Graph</Text>
+              <BarChart
+               width={190}
+               height={190}
+                data={data}
+                chartConfig={chartConfig}
+                style={graphStyle}
+              />
+              <Text style={labelStyle}>Pie Chart</Text>
+              <PieChart
+                data={pieChartData}
+                width={190}
+                height={190}
+                chartConfig={chartConfig}
+                accessor="population"
+                style={graphStyle}
+              />
+              <Text style={labelStyle}>Line Chart</Text>
+              <LineChart
+                data={data}
+                width={190}
+                height={190}
+                chartConfig={chartConfig}
+                style={graphStyle}
+              />
+              <Text style={labelStyle}>Contribution Graph</Text>
+              <ContributionGraph
+                values={contributionData}
+                width={190}
+                height={190}
+                endDate={new Date('2016-05-01')}
+                numDays={105}
+                chartConfig={chartConfig}
+                style={graphStyle}
+              />
+            </ScrollView>
+          )
+        })}
+        {/* </View> */}
+        {/* <ScrollView >
+            {[1, 2, 3, 4, 5]?.map((item, index) => {
+              return (
+                <View
+                  key={index}
+                  className="flex flex-row items-center justify-around p-3 mt-1.5 bg-white shadow rounded-xl">
+                  <View className="bg-[#FDEEE3] h-[50px] w-[50px] rounded-full border border-[#FDD7BC] p-3">
+                    <Image
+                      style={{height: 22, width: 29.5}}
+                      source={require('../Assets/image/drawable-xhdpi/pngwing_com_9.png')}
+                    />
+                  </View>
+                  <View>
+                    <Text
+                      className="text-[#5a607f] text-[13px]"
+                      style={{fontFamily: 'Poppins-Regular'}}>
+                      Product name
+                    </Text>
+                    <View className="flex flex-row gap-x-2">
+                      <Text
+                        className="text-[#5a607f] bg-[#e6e9f4] text-center h-[12px] rounded-full w-[55px] text-[7px]"
+                        style={{fontFamily: 'Poppins-SemiBold'}}>
+                        SKU : 575
+                      </Text>
+                      <Text
+                        className="text-[#5a607f] bg-[#e6e9f4] text-center h-[12px] rounded-full w-[55px] text-[7px]"
+                        style={{fontFamily: 'Poppins-SemiBold'}}>
+                        Grocery5
+                      </Text>
+                    </View>
+                  </View>
+                  <View>
+                    <Text
+                      className="text-[#f96900] pb-3 text-[13px]"
+                      style={{fontFamily: 'Poppins-Medium'}}>
+                      50 AED
+                    </Text>
+                  </View>
+                  <View>
+                    <Entypo
+                      name="dots-three-vertical"
+                      size={20}
+                      color="#cbcbcb"
+                    />
+                  </View>
+                </View>
+              );
+            })}
+          
+        </ScrollView> */}
+        {/* <View>
+          <TouchableOpacity
+            className="mt-2 rounded-full"
+            // onPress={() => redirect()}
+            style={styles.button}>
+            <Text
+              className="text-white"
+              style={{fontFamily: 'Poppins-SemiBold'}}>
+              ADD PRODUCT
+            </Text>
+          </TouchableOpacity>
+        </View> */}
+      </View>
     </View>
   );
 }
@@ -215,7 +609,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     // marginHorizontal: 10,
     borderBottomWidth: 0,
-    marginTop: 10,
   },
   input: {
     flex: 1,
