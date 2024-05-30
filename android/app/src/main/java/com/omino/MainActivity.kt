854430090b8a159b2a924e9a42ebef38c8abccb8
begumfathima992 <87388838+begumfathima992@users.noreply.github.com>
2024-05-30 17:24:02 +0530

    drawer exist but build failes

--- android/app/src/main/java/com/omino/MainActivity.kt
+++ android/app/src/main/java/com/omino/MainActivity.kt
@@ -5,23 +5,22 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-class MainActivity : ReactActivity() {
+public class MainActivity extends ReactActivity {
 
-   override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
-  }
+    @Override
+    protected String getMainComponentName() {
+        return "omino";
+    }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "omino"
-
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
 }
